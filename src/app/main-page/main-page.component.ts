2daf13c5a094de1a654680303578e4491e439a3b
venkat <venkataramarajupr@gmail.com>
2021-02-10 22:45:59 +0530

    timmer added

---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.scss']
+})
+export class MainPageComponent implements OnInit {
+
+  countDownDate = new Date("May 27, 2021 01:45:00").getTime();
+  now: any;
+  distances: any;
+  days: any;
+  hours: any;
+  minutes: any;
+  seconds: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    setInterval(() => this.settime(),100);
+  }
+
+  settime() {
+    this.now = new Date().getTime();
+    this.distances = this.countDownDate - this.now;
+    this.days = Math.floor(this.distances / (1000 * 60 * 60 * 24));
+    this.hours = Math.floor((this.distances % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    this.minutes = Math.floor((this.distances % (1000 * 60 * 60)) / (1000 * 60));
+    this.seconds = Math.floor((this.distances % (1000 * 60)) / 1000);
+    // if (distance < 0) {
+    //   clearInterval(x);
+    //   // document.getElementById("demo").innerHTML = "EXPIRED";
+    // }
+    console.log(this.days)
+  }
+  
+
+}
